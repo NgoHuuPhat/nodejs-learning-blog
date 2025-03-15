a7eaeff19ec03d81e98c63e4264d4ce83ace48b4
NgoHuuPhat <ngoohuuphat@gmail.com>
2025-03-15 22:04:08 +0700

    Update collection roles

--- src/app/models/Role.js
+++ src/app/models/Role.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose')
-const mongooseDelete = require('mongoose-delete')
 const Schema = mongoose.Schema
 
 const RoleSchema = new Schema(
     {
         name: { type: String, required: true },
         description: { type: String, maxLength: 600 },
+        permissions: { type: [String], default: [] } // Đảm bảo phần tử phải là String
     }, 
-    { 
-        timestamps: true 
-    }
+    { timestamps: true }
 )
 
-// { overrideMethods: 'all' } Thay thế các phương thức bằng phương thức xóa mềm
-RoleSchema.plugin(mongooseDelete, { 
-    deletedAt : true,
-    overrideMethods: 'all',
-})
-
-module.exports = mongoose.model('Role', RoleSchema ) //Collection - Schema
+module.exports = mongoose.model('Role', RoleSchema) //Collection - Schema
