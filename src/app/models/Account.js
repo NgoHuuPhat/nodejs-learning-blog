d804d8169b2a6afebc8e302d0c79b677b5275ebf
NgoHuuPhat <ngoohuuphat@gmail.com>
2025-03-19 23:49:34 +0700

    Add collection account

---
+++ src/app/models/Account.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { sortTable } = require('../../helpers/queryHelper'); 
+
+const AccountSchema = new Schema(
+    {
+        fullName: { type: String, required: true},
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        token: { type: String, default: ""},
+        phone: { type: String},
+        avatar: { type: String},
+        role_id: { type: String, required: true },
+        status: { type: String, required: true }
+    }, 
+    { timestamps: true }
+);
+
+// Sử dụng query helper chung
+AccountSchema.query.sortTable = sortTable;
+
+module.exports = mongoose.model('Account', AccountSchema); // Collection - Schema
