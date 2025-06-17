2539b9be6f809725c56ed42bf40f697f2391ba57
NgoHuuPhat <ngoohuuphat@gmail.com>
2025-06-17 15:55:32 +0700

    feat: develop delete course

--- src/app/controllers/admin/CourseController.js
+++ src/app/controllers/admin/CourseController.js
@@ -4,6 +4,8 @@ const Chapter = require('../../models/Chapter')
 const Lesson = require('../../models/Lesson')
 const paginatitonHelper = require('../../../helpers/pagination')
 const { formatDate } = require('../../../helpers/format')
+const { uploadCloudinary } = require('../../middlewares/uploadCloudinary')
+const { image } = require('../../../config/cloudinary')
 
 class CourseController {
     //[GET] /admin/courses
@@ -80,6 +82,7 @@ class CourseController {
                     url: req.uploadResults.courseVideoPreview[0].secure_url,
                     duration: req.uploadResults.courseVideoPreview[0].duration,
                 }
+                console.log('videoPreview:', videoPreview)
             }
             
             const courseStructure = JSON.parse(req.body.courseStructure)
@@ -125,19 +128,22 @@ class CourseController {
                 const chapterId = savedChapters[index]._id 
 
                 chapter.lessons.forEach((lesson) => {
+                    // Kiểm tra videoLesson có dữ liệu không, nếu không thì để rỗng
+                    const videoLesson = courseVideoLesson[indexLesson] || {}
+                
                     lessons.push({
                         chapter_id: chapterId,
                         title: lesson.title,
                         videoLesson: {
-                            video_id: courseVideoLesson[indexLesson].public_id,
-                            url: courseVideoLesson[indexLesson].secure_url,
-                            duration: courseVideoLesson[indexLesson].duration,
+                            video_id: videoLesson.public_id || '',
+                            url: videoLesson.secure_url || '',
+                            duration: videoLesson.duration || 0,
                         },
                         createdBy: {
                             account_id: res.locals.account.id,
                             createdAt: new Date(),
                         },
-                    })
+                    })  
                     indexLesson++
                 })
             })
@@ -193,10 +199,11 @@ class CourseController {
         }
     }
 
+
     //[DELETE] /admin/courses/:id
     async destroy(req, res, next) {
         try {
-            //Xóa mềm lưu thông tin người xóa
+            //Xóa mềm lưu thông tin người xóa khóa học
             await Course.updateOne(
                 { _id: req.params.id },
                 {
@@ -207,8 +214,35 @@ class CourseController {
                 },
             )
 
-            //Chính thức xóa mềm
+            // Xóa mềm lưu thông tin người xóa chương học
+            await Chapter.updateMany(
+                { course_id: req.params.id },
+                {
+                    deletedBy: {
+                        account_id: res.locals.account.id,
+                        deletedAt: new Date(),
+                    },
+                },
+            )
+            
+            // Xóa mềm lưu thông tin người xóa bài học
+            const chapters = await Chapter.find({ course_id: req.params.id }).lean()
+            const chapterIds = chapters.map(chapter => chapter._id)
+            await Lesson.updateMany(
+                { chapter_id: { $in: chapterIds } },
+                {
+                    deletedBy: {
+                        account_id: res.locals.account.id,
+                        deletedAt: new Date(),
+                    },
+                },
+            )
+
+            //Chính thức xóa mềm 
             await Course.delete({ _id: req.params.id })
+            await Chapter.delete({ course_id: req.params.id })
+            await Lesson.delete({ chapter_id: { $in: chapterIds } })
+            
             res.redirect(req.get('Referrer') || '/') //'back' về lại trang trước đó
         } catch (error) {
             next(error)
@@ -218,7 +252,14 @@ class CourseController {
     //[PATCH] /admin/courses/:id/restore
     async restore(req, res, next) {
         try {
+
             await Course.restore({ _id: req.params.id })
+            await Chapter.restore({ course_id: req.params.id })
+
+            const chapters = await Chapter.find({ course_id: req.params.id }).lean()
+            const chapterIds = chapters.map(chapter => chapter._id)
+            await Lesson.restore({ chapter_id: { $in: chapterIds } })
+
             res.redirect(req.get('Referrer') || '/') //'back' về lại trang trước đó
         } catch (error) {
             next(error)
@@ -243,9 +284,15 @@ class CourseController {
     }
 
     //[DELETE] /admin/courses/:id/force
-    async forceDestroy(req, res, next) {
+    async forceDestroy(req, res, next)  {
         try {
+            const chapters = await Chapter.find({ course_id: req.params.id }).lean()
+            const chapterIds = chapters.map(chapter => chapter._id)
+
+            await Lesson.deleteMany({ chapter_id: { $in: chapterIds } })
             await Course.deleteOne({ _id: req.params.id })
+            await Chapter.deleteMany({ course_id: req.params.id })
+
             res.redirect(req.get('Referrer') || '/') //'back' về lại trang trước đó
         } catch (error) {
             next(error)
